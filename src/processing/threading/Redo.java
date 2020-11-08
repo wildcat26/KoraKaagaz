e5ae20a3cf21b4ea580618bd4ad7abf4cac3a153
Shruti Umat <111701027@smail.iitpkd.ac.in>
2020-11-09 01:41:02 +0530

    Wrapper for Undo Redo operations

--- src/processing/threading/Redo.java
+++ src/processing/threading/Redo.java
@@ -1,11 +1,16 @@
 package processing.threading;
 
+import processing.UndoRedo;
+
 /**
+ * Wrapper class implementing Runnable interface for threading of the redo operation
+ *
  * @author Shruti Umat
  */
 
 public class Redo implements Runnable {
+
     public void run() {
-        return;
+        UndoRedo.redo();
     }
 }
--- src/processing/threading/Undo.java
+++ src/processing/threading/Undo.java
@@ -1,11 +1,16 @@
 package processing.threading;
 
+import processing.UndoRedo;
+
 /**
+ * Wrapper class implementing Runnable interface for threading of the undo operation
+ *
  * @author Shruti Umat
  */
 
 public class Undo implements Runnable {
+
     public void run() {
-        return;
+        UndoRedo.undo();
     }
 }
